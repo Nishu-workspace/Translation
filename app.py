c7084127a9fb6f9769dbbd191cd00514626b95e7
Nishu <82433193+Nishu-workspace@users.noreply.github.com>
2025-01-01 19:54:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
 model_path = "D:/Projects/Translation/tf_model"
 # Load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained('tf_model')
+tokenizer = AutoTokenizer.from_pretrained("tf_model")
 model = TFAutoModelForSeq2SeqLM.from_pretrained("tf_model")
 
 # Streamlit page configuration
